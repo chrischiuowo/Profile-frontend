e4af21d0470135649ff6dfd58b1e0ad3c499141d
Chris <81079621+chrisstar30@users.noreply.github.com>
2022-09-06 09:53:58 +0800

    Test: Heroku

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    buildAssetsDir: '/chris/'
+  },
   css: ['@/assets/style/style.sass'],
   plugins: [
     { src: '@/plugins/aos', mode: 'client' }
